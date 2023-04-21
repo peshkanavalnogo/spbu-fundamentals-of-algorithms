57445fcb53fad688d851ac1cdeca5437df193139
peshkanavalnogo <playvladosik@gmail.com>
2023-04-22 00:23:46 +0300

    solved prac 6

--- practicum_6/homework/binary_tree_zigzag_level_order_traversal.py
+++ practicum_6/homework/binary_tree_zigzag_level_order_traversal.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from typing import Any
+from queue import *
+from collections import *
 
 import yaml
 
@@ -20,23 +22,43 @@ class BinaryTree:
     def empty(self) -> bool:
         return self.root is None
 
-    def zigzag_level_order_traversal(self) -> list[Any]:
-
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
-
+    def zigzag_level_order_traversal(self, root) -> list[Any]:
+        if not root: return []
+        ans = defaultdict(list)
+        queue = deque([(root, 0)])
+        while queue:
+            node, depth = queue.popleft()
+            if depth % 2 == 0:
+                ans[depth].append(node.key)
+            else:
+                ans[depth].insert(0, node.key)
+            if node.left: queue.append((node.left, depth + 1))
+            if node.right: queue.append((node.right, depth + 1))
+        return list(ans.values())
 
 def build_tree(list_view: list[Any]) -> BinaryTree:
     bt = BinaryTree()
+    if list_view:
+        node = Node(key=list_view[0])
+        bt.root = node
 
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+        nodes = [node]
+        for i, x in enumerate(list_view[1:]):
+            if x is None:
+                continue
+            parent = nodes[i // 2]
+            new_node = Node(key=x)
+            new_node.key = x
+            if i % 2 == 0:
+                parent.left = new_node
+            else:
+                parent.right = new_node
+            nodes.append(new_node)
+    else:
+        bt.root = None
 
-    pass
+    return bt
+
 
 
 if __name__ == "__main__":
@@ -46,12 +68,10 @@ if __name__ == "__main__":
     # Second, implement BinaryTree.zigzag_traversal() returning the list required by the task
     # Avoid recursive traversal!
 
-    with open(
-        "practicum_6/homework/binary_tree_zigzag_level_order_traversal_cases.yaml", "r"
-    ) as f:
+    with open("binary_tree_zigzag_level_order_traversal_cases.yaml", "r") as f:
         cases = yaml.safe_load(f)
 
     for i, c in enumerate(cases):
         bt = build_tree(c["input"])
-        zz_traversal = bt.zigzag_level_order_traversal()
+        zz_traversal = bt.zigzag_level_order_traversal(bt.root)
         print(f"Case #{i + 1}: {zz_traversal == c['output']}")
--- practicum_6/homework/xor_list.py
+++ practicum_6/homework/xor_list.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 from typing import Any
+import ctypes
 
 import yaml
 
@@ -9,53 +10,56 @@ import yaml
 class Element:
     key: Any
     data: Any = None
-    np: int = None
+    np: int = 0
 
-    def next(self) -> Element:
+    def next(self, prev_np) -> int:
+        next_np = self.np ^ prev_np
+        return next_np
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
 
-        pass
-
-    def prev(self) -> Element:
-
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
+    def prev(self, next_np) -> int:
+        prev_np = self.np ^ next_np
+        return prev_np
 
 
 class XorDoublyLinkedList:
     def __init__(self) -> None:
         self.head: Element = None
+        self.tail: Element = None
+        self.nodes = []
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
 
     def __repr__(self) -> str:
         return str(self)
 
     def __str__(self) -> str:
-        node_keys = []
+        nodes = []
+        node = self.head
+        prev_np = 0
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        return " <-> ".join(node_keys)
+        while node:
+            nodes.append(str(node.key))
+            next_np = prev_np ^ node.np
+            if next_np:
+                prev_np = id(node)
+                node = ctypes.cast(next_np, ctypes.py_object).value
+            else:
+                break
+        return " <-> ".join(nodes)
 
     def to_pylist(self) -> list[Any]:
         py_list = []
-        next_el: Element = self.head()
-        while next_el is not None:
-            py_list.append(next_el.key)
-            next_el = next_el.next()
+        node = self.head
+        prev_np = 0
+
+        while node:
+            py_list.append(node.key)
+            next_np = prev_np ^ node.np
+            if next_np:
+                prev_np = id(node)
+                node = ctypes.cast(next_np, ctypes.py_object).value
+            else:
+                break
         return py_list
 
     def empty(self):
@@ -63,44 +67,69 @@ class XorDoublyLinkedList:
 
     def search(self, k: Element) -> Element:
         """Complexity: O(n)"""
+        if self.empty():
+            return "This list is empty"
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
+        next_np = id(self.head)
+        prev_np = 0
+        next_element = self.head
 
-        pass
+        while next_np != 0 and next_element.key != k.key:
+            prev_np, next_np = next_np, next_element.next(prev_np)
+            if next_id == 0:
+                return "This list doesn't store this element"
+            next_element = ctypes.cast(next_np, ctypes.py_object).value
+        return next_element
 
     def insert(self, x: Element) -> None:
         """Insert to the front of the list (i.e., it is 'prepend')
         Complexity: O(1)
         """
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
+        if self.empty():
+            self.head = self.tail = x
+        else:
+            x.np = id(self.head)
+            self.head.np = self.head.np ^ id(x)
+            self.head = x
+        self.nodes.append(x)
 
     def remove(self, x: Element) -> None:
         """Remove x from the list
         Complexity: O(1)
         """
+        if self.empty():
+            return
 
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
+        if not x:
+            return
 
-        pass
+        next_np = id(self.head)
+        prev_np = 0
+        next_element = ctypes.cast(next_np, ctypes.py_object).value
+        while next_np != 0 and next_element.key != x.key:
+            prev_np, next_np = next_np, next_element.next(prev_np)
+            next_element = ctypes.cast(next_np, ctypes.py_object).value
+        if next_np != 0:
+            next_next_np = next_element.np ^ prev_np
+            if prev_np != 0:
+                prev_element = ctypes.cast(prev_np, ctypes.py_object).value
+                prev_element.np = prev_element.np ^ next_np ^ next_next_np
+            else:
+                self.head = next_next_element
+            if next_next_np != 0:
+                next_next_element = ctypes.cast(next_next_np, ctypes.py_object).value
+                next_next_element.np = next_next_element.np ^ next_np ^ prev_np
+            else:
+                self.tail = prev_element
+            self.nodes.remove(next_element)
+
 
     def reverse(self) -> XorDoublyLinkedList:
         """Returns the same list but in the reserved order
         Complexity: O(1)
         """
-
-        ##########################
-        ### PUT YOUR CODE HERE ###
-        ##########################
-
-        pass
+        self.head, self.tail = self.tail, self.head
+        return self
 
 
 if __name__ == "__main__":
@@ -112,20 +141,24 @@ if __name__ == "__main__":
     # >>> ctypes.cast(id(a), ctypes.py_object).value
     # Hint: assuming that self.next and self.prev store pointers
     # define self.np as self.np = self.next XOR self.prev
-
-    with open("practicum_6/homework/xor_list_cases.yaml", "r") as f:
+    with open("xor_list_cases.yaml", "r") as f:
         cases = yaml.safe_load(f)
 
     for i, c in enumerate(cases):
         l = XorDoublyLinkedList()
         for el in reversed(c["input"]["list"]):
-            l.insert(el)
+            l.insert(Element(key=el))
+        print(l)
         for op_info in c["input"]["ops"]:
             if op_info["op"] == "insert":
-                l.remove(op_info["key"])
+                l.insert(Element(key=op_info["key"]))
+                print(l)
             elif op_info["op"] == "remove":
-                l.remove(op_info["key"])
+                l.remove(Element(op_info["key"]))
+                print(l)
             elif op_info["op"] == "reverse":
                 l = l.reverse()
+                print(l)
         py_list = l.to_pylist()
+        print(py_list)
         print(f"Case #{i + 1}: {py_list == c['output']}")
